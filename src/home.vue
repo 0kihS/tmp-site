a2cad532c6fa2b3c9af3f51fcb819a24036136bb
0kihS <teampokemonleet@gmail.com>
2024-04-30 22:16:25 +0200

    rewrite homepage in typescript

--- src/home.vue
+++ src/home.vue
@@ -1,21 +1,21 @@
-<script setup>
+<script setup lang="ts">
 import { Application } from '@splinetool/runtime';
 import { ref, reactive, onMounted } from 'vue';
 
 // template ref
-const canvas = ref(null)
+const canvas = ref<HTMLCanvasElement | null>(null)
 
 // spline state
 const state = reactive({
     spline: {
         scene: "https://prod.spline.design/zoBeB2JCtkuRMSN0/scene.splinecode",
-        app: null,
+        app: null as Application | null,
         isLoaded: false,
     },
 });
 
 onMounted(async () =>{
-    const app = new Application(canvas.value);
+    const app = new Application(canvas.value as HTMLCanvasElement);
     await app
     .load(state.spline.scene)
 
