16468b2b916996e9af698079651910b90c83dbbf
0kihS <teampokemonleet@gmail.com>
2024-05-06 19:59:45 +0200

    show card stats

--- src/search.vue
+++ src/search.vue
@@ -118,6 +118,8 @@ type CardData = {
   cardtype: string; 
   type: string; 
   level: string; 
+  atk: number; 
+  def: number; 
 };
 let cdata: Ref<CardData[]> = ref([]);
 
@@ -528,6 +530,7 @@ const toggleLinkArrow = (direction: string) => {
            {{ card.cardtype }} <h4 class="inline" v-if="card.attribute">{{ card.attribute.toUpperCase() }}</h4> <h4 class="inline" v-if="card.type.includes('XYZ')"> {{ 'Rank ' + card.level }}</h4><h4 class="inline" v-else-if="card.type.includes('LINK')"> {{ 'LINK-' + card.level }}</h4><h4 class="inline" v-else-if="card.level"> {{ 'Level ' + card.level }}</h4></h4>
         <h4 v-if="card.type" class="text-sm font-medium">{{ card.type.charAt(0).toUpperCase() + card.type.slice(1) }}</h4> 
         <p class="text-sm" style="white-space: pre-wrap"> {{ card.effect }}</p>
+        <p v-if"card.atk"> {{ card.atk }}</p>
       </div>
     </div>
   </div>
